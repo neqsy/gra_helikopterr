670431044fae8821802cc5965443f024408486b7
neqsy <41023938+neqsy@users.noreply.github.com>
2020-06-09 22:26:56 +0200

    Update helikopterr.py

--- helikopterr.py
+++ helikopterr.py
@@ -1,4 +1,5 @@
-import os
+""" Moduł zawiera grę helikopterr"""
+
 import math
 import random
 import pygame
@@ -20,8 +21,133 @@ def napisz(tekst, x, y, rozmiar, screen):
     """Funkcja służy do umieszczania napisów w grze."""
 
     czcionka = pygame.font.SysFont("Arial", rozmiar)
-    rend = czcionka.render(tekst, 1, Colors.CZERWONY)
-    screen.blit(rend, (x, y))
+    rysuj = czcionka.render(tekst, 1, Colors.CZERWONY)
+    screen.blit(rysuj, (x, y))
+
+
+def wyswietl_menu(screen):
+    """Funkcja służy do wyświetlania menu w grze."""
+
+    napisz("Naciśnij spację, aby zacząć", 90, 350, WYSOKOSC_TEKSTU, screen)
+    napisz("STEROWANIE: strzałka do góry albo w dół", 90, 390, WYSOKOSC_TEKSTU, screen)
+    grafika = pygame.image.load("logo.png")
+    screen.blit(grafika, (90, 150))
+
+
+def tworzenie_obiektow():
+    """Funkcja służy do tworzenia obiektów(przeszkody, bomby, helikopter, tarcze) menu w grze."""
+    gracz = Helikopter(270, 275)
+    przeszkody = []
+    bomby = []
+    tarcze = []
+
+    for i in range(21):
+        przeszkody.append(Przeszkoda(i * SZEROKOSC / 20, SZEROKOSC / 20))
+    for i in range(1):
+        bomby.append(Sprite(BOMBA, i * SZEROKOSC))
+    for i in range(1):
+        tarcze.append(Sprite(TARCZA, i * SZEROKOSC))
+
+    ruch_y = 0
+    punkty = 0
+    return ruch_y, punkty, gracz, przeszkody, bomby, tarcze
+
+
+def wyswietl_rozgrywka(ruch_y, punkty, gracz, przeszkody, bomby, tarcze, screen):
+    """Funkcja służy do wyświetlania rozgrywki w grze."""
+
+    for p in przeszkody:  # rysowanie przeszkód
+        p.ruch(0.5)
+        p.rysuj(screen)
+        if p.kolizja(gracz.ksztalt):
+            stan_gry = KONIEC
+            return stan_gry
+
+    for p in przeszkody:  # sprawdzam czy przeszkoda miesci sie poza ekranem
+        if p.wspolrzedna_x <= -p.szerokosc:
+            przeszkody.remove(p)
+            przeszkody.append((Przeszkoda(SZEROKOSC, SZEROKOSC / 20)))
+
+    for b in bomby:
+        b.ruch(0.9)
+        b.rysuj(screen)
+        if b.kolizja(gracz.ksztalt):
+            if not gracz.tarcza:
+                stan_gry = KONIEC
+                return stan_gry
+            else:
+                gracz.tarcza = False
+                bomby.remove(b)
+                bomby.append((Sprite(BOMBA, SZEROKOSC)))
+
+    for b in bomby:  # sprawdzam czy bomba miesci sie poza ekranem
+        if b.wspolrzedna_x <= -b.szerokosc:
+            bomby.remove(b)
+            bomby.append((Sprite(BOMBA, SZEROKOSC)))
+
+    for t in tarcze:  # rysowanie tarcz
+        t.ruch(0.5)
+        t.rysuj(screen)
+        if t.kolizja(gracz.ksztalt):
+            gracz.tarcza = True
+            tarcze.remove(t)
+            tarcze.append((Sprite(TARCZA, SZEROKOSC * 3)))
+
+    for t in tarcze:  # sprawdzam czy tarcza miesci sie poza ekranem
+        if t.wspolrzedna_x <= -t.szerokosc:
+            tarcze.remove(t)
+            tarcze.append((Sprite(TARCZA, SZEROKOSC * 3)))
+
+    gracz.rysuj(screen)
+    gracz.ruch(ruch_y)  # ruch gracza (góra, dół)
+
+    napisz(f"PUNKTY: {round(punkty, 2)}", 50, 50, WYSOKOSC_TEKSTU, screen)
+    stan_gry = ROZGRYWKA
+    return stan_gry
+
+
+def wyswietl_koniec(punkty):
+    """Funkcja służy do wyświetlania tekstu po porażce w grze."""
+
+    screen = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
+    grafika = pygame.image.load("logo.png")
+    screen.blit(grafika, (80, 30))
+    napisz("Niestety przegrywasz", 50, 290, WYSOKOSC_TEKSTU, screen)
+    napisz("Naciśnij spację, aby zagrać ponownie", 50, 350, WYSOKOSC_TEKSTU, screen)
+    napisz(f"Twój wynik to: {str(round(punkty, 2))}", 50, 320, WYSOKOSC_TEKSTU, screen)
+
+
+def wyswietlanie_gry():
+    stan_gry = MENU
+    screen = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
+    ruch_y = 0  # określenie w którym kierunku porusza się helikopter (góra, dół)
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    ruch_y = -0.3
+                if event.key == pygame.K_DOWN:
+                    ruch_y = 0.3
+                if event.key == pygame.K_SPACE:
+                    if stan_gry != ROZGRYWKA:
+                        ruch_y, punkty, gracz, przeszkody, bomby, tarcze = tworzenie_obiektow()
+                        stan_gry = ROZGRYWKA
+
+        screen.fill((0, 0, 0))
+        if stan_gry == MENU:
+            wyswietl_menu(screen)
+        elif stan_gry == ROZGRYWKA:
+            punkty = punkty + math.fabs(ruch_y/10)
+            if ruch_y == 0:
+                punkty = 0
+            stan_gry = wyswietl_rozgrywka(ruch_y, punkty, gracz, przeszkody, bomby, tarcze, screen)
+        elif stan_gry == KONIEC:
+            wyswietl_koniec(punkty)
+        pygame.display.update()
 
 
 class Colors:
@@ -117,100 +243,6 @@ class Sprite:
             return True
         else:
             return False
-
-
-def wyswietlanie_gry():
-    stan_gry = MENU
-    screen = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
-    ruch_y = 0  # określenie w którym kierunku porusza się helikopter (góra, dół)
-    while True:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    ruch_y = -0.3
-                if event.key == pygame.K_DOWN:
-                    ruch_y = 0.3
-                if event.key == pygame.K_SPACE:
-                    if stan_gry != ROZGRYWKA:
-                        gracz = Helikopter(270, 275)
-                        przeszkody = []
-                        bomby = []
-                        tarcze = []
-
-                        for i in range(21):
-                            przeszkody.append(Przeszkoda(i * SZEROKOSC / 20, SZEROKOSC / 20))
-                        for i in range(1):
-                            bomby.append(Sprite(BOMBA, i * SZEROKOSC))
-                        for i in range(1):
-                            tarcze.append(Sprite(TARCZA, i * SZEROKOSC))
-
-                        ruch_y = 0
-                        stan_gry = ROZGRYWKA
-                        punkty = 0
-        screen.fill((0, 0, 0))
-        if stan_gry == MENU:
-            napisz("Naciśnij spację, aby zacząć", 90, 350, WYSOKOSC_TEKSTU, screen)
-            napisz("STEROWANIE: strzałka do góry albo w dół", 90, 390, WYSOKOSC_TEKSTU, screen)
-            grafika = pygame.image.load("logo.png")
-            screen.blit(grafika, (90, 150))
-
-        elif stan_gry == ROZGRYWKA:
-
-            for p in przeszkody:  # rysowanie przeszkód
-                p.ruch(0.5)
-                p.rysuj(screen)
-                if p.kolizja(gracz.ksztalt):
-                    stan_gry = KONIEC
-
-            for p in przeszkody:  # sprawdzam czy przeszkoda miesci sie poza ekranem
-                if p.wspolrzedna_x <= -p.szerokosc:
-                    przeszkody.remove(p)
-                    przeszkody.append((Przeszkoda(SZEROKOSC, SZEROKOSC / 20)))
-                    punkty = punkty + math.fabs(ruch_y)
-
-            for b in bomby:
-                b.ruch(0.9)
-                b.rysuj(screen)
-                if b.kolizja(gracz.ksztalt):
-                    if not gracz.tarcza:
-                        stan_gry = KONIEC
-                    else:
-                        gracz.tarcza = False
-                        bomby.remove(b)
-                        bomby.append((Sprite(BOMBA, SZEROKOSC)))
-
-            for b in bomby:  # sprawdzam czy bomba miesci sie poza ekranem
-                if b.wspolrzedna_x <= -b.szerokosc:
-                    bomby.remove(b)
-                    bomby.append((Sprite(BOMBA, SZEROKOSC)))
-            for t in tarcze:  # rysowanie tarcz
-                t.ruch(0.5)
-                t.rysuj(screen)
-                if t.kolizja(gracz.ksztalt):
-                    gracz.tarcza = True
-                    tarcze.remove(t)
-                    tarcze.append((Sprite(TARCZA, SZEROKOSC*3)))
-
-            for t in tarcze:  # sprawdzam czy tarcza miesci sie poza ekranem
-                if t.wspolrzedna_x <= -t.szerokosc:
-                    tarcze.remove(t)
-                    tarcze.append((Sprite(TARCZA, SZEROKOSC*3)))
-
-            gracz.rysuj(screen)
-            gracz.ruch(ruch_y)  # ruch gracza (góra, dół)
-
-            napisz(f"PUNKTY: {round(punkty, 2)}", 50, 50, WYSOKOSC_TEKSTU, screen)
-        elif stan_gry == KONIEC:
-            grafika = pygame.image.load("logo.png")
-            screen.blit(grafika, (80, 30))
-            napisz("Niestety przegrywasz", 50, 290, WYSOKOSC_TEKSTU, screen)
-            napisz("Naciśnij spację, aby zagrać ponownie", 50, 350, WYSOKOSC_TEKSTU, screen)
-            napisz(f"Twój wynik to: {str(round(punkty, 2))}", 50, 320, WYSOKOSC_TEKSTU, screen)
-        pygame.display.update()
 
 
 def main():
